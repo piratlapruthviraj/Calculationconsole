30ac112f071ac0f6492f7a416405c170a5193dce
prudhvi <prudhvi@PRUDHVIRAJ>
2026-02-20 20:41:53 +0530

    20Feb

--- Calculationconsole/CVForm.Designer.cs
+++ Calculationconsole/CVForm.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CVForm));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
             this.Save = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
@@ -678,35 +678,35 @@
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle13.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle13.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle13.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle13.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle13.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle13;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle14.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle14.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle14.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle14.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle14;
             this.dataGridView1.GridColor = System.Drawing.Color.White;
             this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.dataGridView1.Location = new System.Drawing.Point(573, 247);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle15.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle15.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle15.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle15.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle15;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(610, 260);
@@ -725,40 +725,41 @@
             // dataGridView2
             // 
             this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle16.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle16.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle16.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle16.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle16.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle16;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle17;
             this.dataGridView2.GridColor = System.Drawing.SystemColors.HighlightText;
             this.dataGridView2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.dataGridView2.Location = new System.Drawing.Point(135, 540);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle18;
             this.dataGridView2.RowHeadersWidth = 62;
             this.dataGridView2.RowTemplate.Height = 28;
             this.dataGridView2.Size = new System.Drawing.Size(1048, 100);
             this.dataGridView2.TabIndex = 62;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // Button_save
             // 
@@ -774,7 +775,7 @@
             // Cancel
             // 
             this.Cancel.BackColor = System.Drawing.SystemColors.Highlight;
-            this.Cancel.Location = new System.Drawing.Point(1028, 202);
+            this.Cancel.Location = new System.Drawing.Point(995, 202);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(75, 30);
             this.Cancel.TabIndex = 47;
--- Calculationconsole/CVForm.cs
+++ Calculationconsole/CVForm.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +9,7 @@ namespace Calculationconsole
 {
     public partial class CVForm : Form
     {
+        string conString = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
         public CVForm()
         {
             InitializeComponent();
@@ -28,10 +24,35 @@ namespace Calculationconsole
         {
             dataGridView1.AllowUserToAddRows = false;
             dataGridView2.AllowUserToAddRows = false;
+            if (!dataGridView2.Columns.Contains("EditColumn"))
+            {
+                var editCol = new DataGridViewButtonColumn
+                {
+                    Name = "EditColumn",
+                    HeaderText = "Edit",
+                    Text = "Edit",
+                    UseColumnTextForButtonValue = true,
+                    AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+                };
+                dataGridView2.Columns.Add(editCol);
+            }
+
+            if (!dataGridView2.Columns.Contains("DeleteColumn"))
+            {
+                var deleteCol = new DataGridViewButtonColumn
+                {
+                    Name = "DeleteColumn",
+                    HeaderText = "Delete",
+                    Text = "Delete",
+                    UseColumnTextForButtonValue = true,
+                    AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+                };
+                dataGridView2.Columns.Add(deleteCol);
+            }
             string conString = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
             using (SqlConnection con = new SqlConnection(conString))
             {
-                string sql = "SELECT Prefix, FullName, Title, DPT.DEPARTMENTNAME, CAST(Hiredate AS DATE) Hiredate, MobilePhone, Email, Address " +
+                string sql = "SELECT EMPID, Prefix, FullName, Title, DPT.DEPARTMENTNAME, CAST(Hiredate AS DATE) Hiredate, MobilePhone, Email, Address " +
                     "FROM dbo.EMPLOYEE EMP(NOLOCK), dbo.DEPARTMENT DPT(NOLOCK) " +
                     "WHERE EMP.DPTID = DPT.DPTID";
                 SqlDataAdapter da = new SqlDataAdapter(sql, con);
@@ -86,7 +107,10 @@ namespace Calculationconsole
 
         private void Button_save_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrWhiteSpace(textBox1.Text))
+            Button_save.Enabled = false;
+            string DepartmentName = comboBox4.Text.Trim();
+            int DeptID = GetDepartmentID(DepartmentName);
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
             {
                 MessageBox.Show("First Name is Required");
                 textBox1.Focus();
@@ -99,14 +123,13 @@ namespace Calculationconsole
                 return;
             }
 
-            string conString = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
             using (SqlConnection con = new SqlConnection(conString))
             {
                 string sql = @"INSERT INTO dbo.EMPLOYEE (FIRSTNAME, LASTNAME, FULLNAME, BIRTHDATE, TITLE, PREFIX,
                                 ADDRESS, CITY, STATE, ZIPCODE, HOMEPHONE, MOBILEPHONE, EMAIL, SKYPE, HIREDATE, DPTID) 
                                 VALUES
                                 (@FirstName, @LastName, @FullName, @BirthDate, @Title, @prefix,
-                                 @Address, @city, @state, @ZipCode, @HomePhone, @MobilePhone, @Email, @Skype, @HireDate, DPTID)";
+                                 @Address, @city, @state, @ZipCode, @HomePhone, @MobilePhone, @Email, @Skype, @HireDate, @DPTID)";
 
                 using (SqlCommand cmd = new SqlCommand(sql, con))
                 {
@@ -125,6 +148,7 @@ namespace Calculationconsole
                     cmd.Parameters.AddWithValue("@Email", textBox10.Text.Trim());
                     cmd.Parameters.AddWithValue("@Skype", textBox11.Text.Trim());
                     cmd.Parameters.AddWithValue("@HireDate", dateTimePicker2.Value.Date);
+                    cmd.Parameters.AddWithValue("@DPTID", DeptID);
 
                     try
                     {
@@ -134,7 +158,7 @@ namespace Calculationconsole
                         CVForm_Load(sender, e);
 
                     }
-                    catch(Exception ex)
+                    catch (Exception ex)
                     {
                         MessageBox.Show("Error: " + ex.Message);
                     }
@@ -149,9 +173,133 @@ namespace Calculationconsole
 
         private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+
         }
 
+        public int GetDepartmentID(string DepartmentName)
+        {
+            int departmentid = 0;
+            string queryString = "SELECT DPTID FROM Department WHERE DepartmentName = @DepartmentName";
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(queryString, con))
+                {
+                    command.Parameters.AddWithValue("@DepartmentName", DepartmentName);
+
+                    try
+                    {
+                        var result = command.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            departmentid = Convert.ToInt32(result.ToString());
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error retrieving department name: " + ex.Message);
+                        // You might want to log the exception or throw it further up
+                    }
+                }
+            }
+
+            return departmentid;
+        }
+
+        private async Task OnDeleteEmployeeAsync(int employeeId)
+        {
+            var confirm = MessageBox.Show(
+                "Are you sure you want to delete this employee?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (confirm != DialogResult.Yes) return;
+
+            try
+            {
+                await DeleteEmployeeAsync(employeeId);
+                //   EnsureActionButtonsOnGrid();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("SQL Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async Task DeleteEmployeeAsync(int employeeId)
+        {
+            const string sql = "DELETE FROM Employee WHERE EMPID = @EmployeeId;";
+
+            using (var con = new SqlConnection(conString))
+            using (var cmd = new SqlCommand(sql, con))
+            {
+                cmd.Parameters.Add("@EmployeeId", SqlDbType.Int).Value = employeeId;
+                await con.OpenAsync();
+                await cmd.ExecuteNonQueryAsync();
+                MessageBox.Show("Employee removed successfully");
+            }
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            dataGridView2.AllowUserToAddRows = false;
+            
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            var grid = (DataGridView)sender;
+            var column = grid.Columns[e.ColumnIndex];
+            if (column.Name != "EditColumn" && column.Name != "DeleteColumn") return;
+
+            var row = grid.Rows[e.RowIndex];
+            if (row?.DataBoundItem == null) return;
+
+            var employeeIdObj = row.Cells["EMPID"]?.Value;
+            if (employeeIdObj == null || employeeIdObj == DBNull.Value) return;
+
+            int employeeId = Convert.ToInt32(employeeIdObj);
+
+            if (column.Name == "DeleteColumn")
+            {
+                _ = OnDeleteEmployeeAsync(employeeId);
+                CVForm_Load(sender, e);
+            }
+
+            else if (column.Name == "EditColumn")
+            {
+                dataGridView2.EditMode = DataGridViewEditMode.EditOnEnter;
+                row = dataGridView2.Rows[e.RowIndex];
+
+                TextBox txt = new TextBox();
+                txt.Text = row.Cells["EMPID"].Value.ToString();
+                txt.Name = row.Cells["FULLNAME"].Value.ToString();
+
+                const string sql = "UPDATE Employee SET FULLNAME = @FULLNAME WHERE EMPID = @EMPID;";
+
+                using (var con = new SqlConnection(conString))
+                using (var cmd = new SqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddWithValue("@FULLNAME", txt.Name);
+                    cmd.Parameters.AddWithValue("@EMPID", txt.Text);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Employee Record Updated Successfully");
+                    CVForm_Load(sender, e);
+                }
+
+            }
+
+        }
 
     }
 }
